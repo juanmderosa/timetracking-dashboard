9c875b3a1025a3615617a0f886d4a97ac24adc32
Juan Martin De Rosa <juanmderosa@gmail.com>
2023-07-04 14:13:15 -0300

    json correction

--- src/components/ReportCard.jsx
+++ src/components/ReportCard.jsx
@@ -9,22 +9,32 @@ const colorSelector = {
     "Self Care": "hsl(43, 84%, 65%)",
   }
 export const ReportCard = ({data, time}) => {
-  console.log(data)
+  const getMessage = (previous) => {
+    if (time === "weekly") {
+      return `Last Week - ${previous}`;
+    } else if (time === "daily") {
+      return `Last Day - ${previous}`;
+    } else if (time === "monthly") {
+      return `Last Month - ${previous}`;
+    }
+    return "";
+  };
+
   return (
     <>
     {data.length > 0 && data.map((item, index)=>(
             <article key={index} className="activity-container">
-            <div className="activity-header" style={{ backgroundColor: colorSelector[item.title] }}>
-                <img src={item.img} alt="activity img" />
-            </div>
-            <aside className="activity-info" >
-                <div className="title-container">
-                <p>{item.title}</p>
-                <img className='ellipsis-img' src="/images/icon-ellipsis.svg" alt="ellipsis" />
-                </div>
-                <h2 className="timeframe-h2">{`${item.timeframes[time].current}`}hrs</h2>
-                <p className="timeframe-last">Last  - {`${item.timeframes[time].previous}`}hrs</p>
-            </aside>
+              <div className="activity-header" style={{ backgroundColor: colorSelector[item.title] }}>
+                  <img src={item.img} alt="activity img" />
+              </div>
+              <aside className="activity-info" >
+                  <div className="title-container">
+                  <p>{item.title}</p>
+                  <img className='ellipsis-img' src="/images/icon-ellipsis.svg" alt="ellipsis" />
+                  </div>
+                  <h2 className="timeframe-h2">{`${item.timeframes[time].current}`}hrs</h2>
+                  <p className="timeframe-last">{getMessage(item.timeframes[time].previous)}</p>
+              </aside>
             </article>
         ))}
     </>
